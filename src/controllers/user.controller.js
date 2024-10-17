a90c93a4cd61812a0d22974035f9ed17ad0239cc
SatyamBhardwaj7576 <bharadwajsatyam88@gmail.com>
2024-10-18 01:47:42 +0530

    add register controller and bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,94 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import {ApiError} from "../utils/ApiError.js"
+import { User } from "../models/user.model.js";
+import {uploadOnCloudinary} from "../utils/cloudinary.js";
+import { upload } from "../middlewares/multer.middleware.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async(req,res)=>{
-    res.status(200).json({
-        message:"User registered"
-    })
+    // res.status(200).json({
+    //     message:"User registered"
+    // })
+
+  //jbb user register karega to kya kya chahiye
+  //get user details from frontend
+  // validation -not empty
+  //check if user already exist ,username ,email
+  //upload them to cloudinary , avatar
+  //create user object -  creating entry in database
+  //remove password and refresh token field from response
+  //check for user creation
+  //return response 
+
+  const {fullName, email,username , password} = req.body;
+  console.log("email :",email);
+  
+//isse bsss full name check kiye hain
+//Aise hi tmm bohot sare if else laga ke sab kch check kar sakte ho
+//   if(fullName === ""){
+//     throw new ApiError(400 , "full name is required");
+    
+//   } agrr aise likhoge to bohot sare if else loop likhna padega 
+// Agrr beginner ho to likh sakte hai
+
+if([fullName,email,username,password].some((field)=>
+field?.trim()==="")){
+    throw new ApiError(400,"All fields are required");
+}
+
+//abb user check karne ka tarika 
+
+const existedUser = User.findOne({
+    $or : [{username},{email}]
+})
+
+if(existedUser){
+    throw new ApiError(409 ,"User with email or username already exists")
+}
+
+//image bagera bhi dekh lete hain
+
+const avatarLocalPath = req.files?.avatar[0]?.path;
+const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+if(!avatarLocalPath){
+    throw new ApiError(400,"Avatar file is required ")
+}
+
+//Abb image ko cloudinary prr upload karenge 
+
+const avatar = await uploadOnCloudinary(avatarLocalPath)
+const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+if(!avatar){
+    throw new ApiError(400,"Avatar file is required ");
+}
+
+//object bnao aurr database me entry maar do
+
+const user = await User.create({
+    fullName,
+    avatar:avatar.url,
+    coverImage:coverImage?.url || "" ,
+    email,
+    password,
+    username : username.toLowerCase()
+})
+
+const createdUser = await User.findById(user._id).select(
+    " -password -refreshToken"
+)
+
+if(!createdUser){
+    throw new ApiError(500,"Something is wrong while registering user")
+}
+
+//finally response ko return karenge 
+
+return res.status(201).json(
+    new ApiResponse(200, createdUser,"User registered successfully")
+)
+
 })
 
 export { registerUser } 
